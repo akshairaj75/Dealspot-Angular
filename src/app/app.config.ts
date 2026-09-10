036b4733bcad30767c78db53f3dd10faf9bcc10d
Akshai raj <akshairaj75@gmail.com>
2026-09-10 16:08:17 +0530

    Redirection new page on top screen

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './core/interceptor/jwt.interceptor';
@@ -9,7 +9,13 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      })
+    ),
     provideHttpClient(withInterceptors([jwtInterceptor]))
   ]
 };
--- src/app/layout/public-layout/public-layout.ts
+++ src/app/layout/public-layout/public-layout.ts
@@ -422,4 +422,8 @@ export class PublicLayoutComponent implements OnInit, OnDestroy {
     this.authService.logout('/');
   }
 
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }
+
 }
